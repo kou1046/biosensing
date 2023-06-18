0a4398bb06df20b358e3d233c87b81309a783938
iwashiro <iwashiro0517@yahoo.co.jp>
2023-06-18 18:57:11 +0900

    バリデーションを厳格化

--- wavesimulator.py
+++ wavesimulator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import abstractmethod, ABCMeta
 from dataclasses import dataclass
+import itertools
 from typing import Callable
 from enum import Enum
 import numpy as np
@@ -22,6 +23,18 @@ class Direction(Enum):
     LEFTTOP = 5
     RIGHTBOTTOM = 6
     LEFTBOTTOM = 7
+
+
+@dataclass(frozen=True)
+class PositiveFloatXY:
+    x: float
+    y: float
+
+    def __post_init__(self):
+        if self.x < 0 or self.y < 0:
+            raise ValueError("正の値しか認めない")
+        if not isinstance(float, self.x) or not isinstance(float, self.y):
+            raise ValueError("floatに変換する必要がある")
 
 
 @dataclass(frozen=True)
@@ -62,6 +75,14 @@ class Obstacles:
     def get_value(self):
         return self.value.copy()
 
+    def xlim(self):
+        obstacle_xs = np.array([obstacle.xs() for obstacle in self.value]).ravel()
+        return min(obstacle_xs), max(obstacle_xs)
+
+    def ylim(self):
+        obstacle_ys = np.array([obstacle.ys() for obstacle in self.value]).ravel()
+        return min(obstacle_ys), max(obstacle_ys)
+
     def create_fig_ax(self):
         fig, ax = plt.subplots()
         for obstacle in self.get_value():
@@ -87,7 +108,7 @@ class Strain(metaclass=ABCMeta):
 
 X = list[int]
 Y = list[int]
-XYIndices = list[X, Y]
+XYIndices = tuple[X, Y]
 
 
 @dataclass(frozen=True)
@@ -121,26 +142,27 @@ class Grid:
         grid_row_last_index = grid_width - 1
         grid_col_last_index = grid_height - 1
 
-        right_indices = [
+        right_indices = (
             ([grid_row_last_index] * (grid_height - 2)),  # 角の処理のため，最初と最後は追加しない -> Xの要素数は grid_height - 2となる. 以降も同様．
             list(range(1, grid_height - 1)),
-        ]
-        left_indices = [
+        )
+        left_indices = (
             ([0] * (grid_height - 2)),
             list(range(1, grid_height - 1)),
-        ]
-        top_indices = [
+        )
+        top_indices = (
             list(range(1, grid_width - 1)),
             ([0] * (grid_width - 2)),
-        ]
-        bottom_indices = [
+        )
+        bottom_indices = (
             list(range(1, grid_width - 1)),
             ([grid_col_last_index] * (grid_width - 2)),
-        ]
-        righttop_indices = [[grid_row_last_index], [0]]
-        lefttop_indices = [[0], [0]]
-        rightbottom_indices = [[grid_row_last_index], [grid_col_last_index]]
-        leftbottom_indices = [[0], [grid_col_last_index]]
+        )
+
+        righttop_indices = ([grid_row_last_index], [0])
+        lefttop_indices = ([0], [0])
+        rightbottom_indices = ([grid_row_last_index], [grid_col_last_index])
+        leftbottom_indices = ([0], [grid_col_last_index])
 
         return {
             direction: indices
@@ -167,6 +189,16 @@ class WaveConditions:
     strain: Strain = None
     reflect_condition: ReflectCondition = ReflectCondition.NEUMANN
 
+    def __post_init__(self):
+        if self.obstacles is None:
+            return
+        obstacle_xmin, obstacle_xmax = self.obstacles.xlim()
+        if obstacle_xmin < 0 or obstacle_xmax > self.grid.width:
+            raise ValueError("障害物のx座標が範囲外")
+        obstacle_ymin, obstacle_ymax = self.obstacles.ylim()
+        if obstacle_ymin < 0 or obstacle_ymax > self.grid.height:
+            raise ValueError("障害物のy座標が範囲外")
+
     def boundary_and_obstacle_indices(self) -> dict[Direction, XYIndices]:
         """
         障害物及び端付近のインデックスを取得する．
@@ -182,18 +214,18 @@ class WaveConditions:
             if obstacle.is_horizontal():
                 ys = obstacle.ys()
                 assert ys[0] == ys[1], "障害物が並行でない"
-                obstacle_index_xlim = (np.array(sorted(obstacle.xs())) / self.grid.h).astype(int)
+                obstacle_index_xlim = (np.array(sorted(obstacle.xs())) // self.grid.h).astype(int)
                 xmin, xmax = obstacle_index_xlim
                 obstacle_indicies_x = list(range(xmin, xmax + 1))
-                obstacle_indicies_y = [int(ys[0] / self.grid.h)] * len(obstacle_indicies_x)
+                obstacle_indicies_y = [int(ys[0] // self.grid.h)] * len(obstacle_indicies_x)
 
             if obstacle.is_vertical():
                 xs = obstacle.xs()
                 assert xs[0] == xs[1], "障害物が垂直でない"
-                obstacle_index_ylim = (np.array(sorted(obstacle.ys())) / self.grid.h).astype(int)
+                obstacle_index_ylim = (np.array(sorted(obstacle.ys())) // self.grid.h).astype(int)
                 ymin, ymax = obstacle_index_ylim
                 obstacle_indicies_y = list(range(ymin, ymax + 1))
-                obstacle_indicies_x = [int(xs[0] / self.grid.h)] * len(obstacle_indicies_y)
+                obstacle_indicies_x = [int(xs[0] // self.grid.h)] * len(obstacle_indicies_y)
 
             if obstacle.is_right():
                 X, Y = boundary_indices[Direction.RIGHT]
@@ -213,6 +245,8 @@ class WaveConditions:
 
         for direction, indicies in self.boundary_and_obstacle_indices().items():
             ax.scatter(*indicies)
+        ax.invert_yaxis()
+        ax.set_aspect("equal")
         return fig, ax
 
 
@@ -346,27 +380,28 @@ class Wave:
         self.time += self.conditions.grid.dt
 
 
-width = 5.0
-height = 3.2
+width = 5
+height = 1
 h = 0.01
 dt = 0.005
 grid = Grid(width, height, h, dt)
 
 obstacle_list = [
-    Obstacle((2, 1), (3, 1), Direction.BOTTOM),
-    Obstacle((3, 1), (3, 2), Direction.RIGHT),
-    Obstacle((3, 2), (2, 2), Direction.TOP),
-    Obstacle((2, 2), (2, 1), Direction.LEFT),
+    Obstacle((2, 0.3), (3, 0.3), Direction.BOTTOM),
+    Obstacle((3, 0.3), (3, 0.5), Direction.RIGHT),
+    Obstacle((3, 0.5), (2, 0.5), Direction.TOP),
+    Obstacle((2, 0.5), (2, 0.3), Direction.LEFT),
 ]
 obstacles = Obstacles(obstacle_list)
 
 wave_conditions = WaveConditions(grid, obstacles)
+fig_, ax_ = wave_conditions.create_fig_ax()
 wave = Wave(wave_conditions)
-wave.input_gauss(0, 1.5, 3)
+wave.input_gauss(0, 0.5, 3)
 ims = []
 fig, ax = plt.subplots()
 for time in np.arange(0, 10 + dt, dt):
     wave.update()
-    ims.append([ax.imshow(wave.values.T, "binary", vmin=-0.01, vmax=0.01)])
+    ims.append([ax.imshow(wave.values.T, "binary")])
 anim = ArtistAnimation(fig, ims, interval=20)
 plt.show()
